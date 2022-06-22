4e272900378cd31fab14f1c3d7f8992862a47187
Albenga96 <albertoferroni.af@gmail.com>
2022-06-22 08:48:44 +0200

    Added training

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from utils.utils import basic_details
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.model_selection import cross_val_score
 
-import inline as inline
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 # %matplotlib inline
@@ -20,3 +22,39 @@ both = pd.concat([train, test], axis=0).reset_index(drop=True)
 both['subject'] = '#' + both['subject'].astype(str)
 
 print(basic_details(both))
+
+activity = both['Activity']
+label_counts = activity.value_counts()
+
+plt.figure(figsize=(12, 8))
+plt.bar(label_counts.index, label_counts)
+
+plt.show()
+
+Data = both['Data']
+Subject = both['subject']
+train = both.copy()
+train = train.drop(['Data', 'subject', 'Activity'], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(train, activity, test_size=0.2, random_state=0)
+
+num_folds = 10
+seed = 0
+scoring = 'accuracy'
+results = {}
+accuracy = {}
+
+model = KNeighborsClassifier(algorithm='auto', n_neighbors=8, p=1, weights='distance')
+
+_ = cross_val_score(model, X_train, y_train, cv=10, scoring=scoring)
+results["GScv"] = (_.mean(), _.std())
+
+model.fit(X_train, y_train)
+y_predict = model.predict(X_test)
+
+accuracy["GScv"] = accuracy_score(y_test, y_predict)
+
+print(classification_report(y_test, y_predict))
+
+cm = confusion_matrix(y_test, y_predict)
+sns.heatmap(cm, annot=True)
